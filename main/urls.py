4a812abf07973a29804789cd6493dec1c9b44161
korchaginks <kkonstantinserg@gmail.com>
2023-11-11 14:45:07 +0300

    Прописал урлы к эндпоинтам привычек

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
 from main.apps import MainConfig
+from main.views import HabitListAPIView, HabitRetrieveAPIView, HabitCreateAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView, PublicHabitListAPIView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-
+    path('habits/', HabitListAPIView.as_view(), name='habits'),
+    path('public_habits/', PublicHabitListAPIView.as_view(), name='public_habits'),
+    path('habit/<int:pk>', HabitRetrieveAPIView.as_view(), name='habit'),
+    path('habit/create/', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('habit/update/<int:pk>', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('habit/delete/<int:pk>', HabitDestroyAPIView.as_view(), name='habit_delete')
 ]
