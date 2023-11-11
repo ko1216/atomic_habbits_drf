5ae670841cbd3d3a5c73d330e78a383ea9c87120
korchaginks <kkonstantinserg@gmail.com>
2023-11-11 14:41:59 +0300

    Создал примерную модель юзера

--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    tg_id = models.BigIntegerField(unique=True, verbose_name='Telegram_id', null=False, default=0)
+    tg_username = models.CharField(max_length=30, verbose_name='Ник в тг', blank=True, null=True)
+
+    USERNAME_FIELD = "tg_id"
+    REQUIRED_FIELDS = []
